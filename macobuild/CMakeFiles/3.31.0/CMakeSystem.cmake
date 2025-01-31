aac52a91dae44fd43446cf285bb52aa937d3f0c4
frostie <mattwiner04@gmail.com>
2025-01-30 21:09:58 -0800

    8

---
+++ macobuild/CMakeFiles/3.31.0/CMakeSystem.cmake
@@ -0,0 +1,15 @@
+set(CMAKE_HOST_SYSTEM "Darwin-24.1.0")
+set(CMAKE_HOST_SYSTEM_NAME "Darwin")
+set(CMAKE_HOST_SYSTEM_VERSION "24.1.0")
+set(CMAKE_HOST_SYSTEM_PROCESSOR "arm64")
+
+include("/Users/frostie/pspdev/psp/share/pspdev.cmake")
+
+set(CMAKE_SYSTEM "Generic-1")
+set(CMAKE_SYSTEM_NAME "Generic")
+set(CMAKE_SYSTEM_VERSION "1")
+set(CMAKE_SYSTEM_PROCESSOR "mips")
+
+set(CMAKE_CROSSCOMPILING "TRUE")
+
+set(CMAKE_SYSTEM_LOADED 1)
